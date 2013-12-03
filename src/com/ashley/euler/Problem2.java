67c4d38f51386e5a47759a2479369759d892a991
Ashley Hicklin <ahicklin@shelterinsurance.com>
2013-12-03 10:32:37 -0600

    change formatting

--- src/com/ashley/euler/Problem2.java
+++ src/com/ashley/euler/Problem2.java
@@ -1,16 +1,7 @@
 package com.ashley.euler;
 
-public class Problem2
-{
-   public static void main(String[] args)
-   {
-      long sum = 0;
-      for (int i = 1; i < 1000; i++)
-      {
-         if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
-      }
-      System.out.println(sum);
-   }
+public class Problem2 {
+	public static void main(String[] args) {
 
+	}
 }
