04b08a77ee52f2f7e0a7d997aa5a9a6c5b0a88f4
Ashley Hicklin <ahicklin@shelterinsurance.com>
2013-12-03 10:21:58 -0600

    Initial import

---
+++ src/com/ashley/euler/Problem2.java
@@ -0,0 +1,16 @@
+package com.ashley.euler;
+
+public class Problem2
+{
+   public static void main(String[] args)
+   {
+      long sum = 0;
+      for (int i = 1; i < 1000; i++)
+      {
+         if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+      }
+      System.out.println(sum);
+   }
+
+}
